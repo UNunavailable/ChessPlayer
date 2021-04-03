5a559184d6f0543659a78142ef7f9f18dbd8e1c7
UNunavailable <idrustam84@gmail.com>
2021-04-03 15:37:10 +0300

    created abstract methods

--- app/src/main/java/com/example/chessplayer/Board.java
+++ app/src/main/java/com/example/chessplayer/Board.java
@@ -1,5 +1,7 @@
 package com.example.chessplayer;
 
+import android.widget.ImageView;
+
 import com.example.chessplayer.figures.aFigure;
 
 public class Board {
@@ -10,9 +12,22 @@ public class Board {
     public static final int QUEEN = 5;
     public static final int KING = 6;
 
+    public ImageView board;
     public aFigure[][] figures = new aFigure[8][8]; // массив фигур
+    public Coords[][] tileCoords = new Coords[8][8]; // массив координат
+
     public void init() {}; // Инициализация доски
     public void lightUp() {}; // подсветка тайла
     public boolean figureWalk() {return true;}; // проверка на возможность хода фигуры
     public void gameEnd() {}; //конец игры
+    public Coords findNearestTile(Coords coords) {return tileCoords[0][0];}; // находит ближайшие координаты тайла
+}
+
+class Coords {
+    public int X;
+    public int Y;
+    Coords(int X, int Y) {
+        this.X = X;
+        this.Y = Y;
+    }
 }
