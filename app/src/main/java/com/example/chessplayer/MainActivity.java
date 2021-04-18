4a33db903ae2194fc18deb49c24feabb3d1595b9
UNunavailable <idrustam84@gmail.com>
2021-04-18 18:22:31 +0300

    added toolbar

--- app/src/main/java/com/example/chessplayer/MainActivity.java
+++ app/src/main/java/com/example/chessplayer/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.chessplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
